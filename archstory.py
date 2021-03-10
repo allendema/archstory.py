5b2fa54a8289aea959f7845749873a232cad249b
Allen <64094914+allendema@users.noreply.github.com>
2021-03-10 23:46:55 +0100

    Detect if there are no Stories
    Detect if there are no Stories
    Better(?) filenames
    Shorter sleep time

--- archstory.py
+++ archstory.py
@@ -13,54 +13,62 @@ print(mix)
 
 path = query
 try:
-	os.mkdir(path)
+    os.mkdir(path)
 except FileExistsError:
-	print("This user has a folder in your System.")
+    print("This user has a folder in your System.")
 
 os.chdir(path)
 
 
-r = requests.get(mix) # Will add headers in the future.
+r = requests.get(mix)
 
 if r.ok:
-	print("\033[1;32;40m The site to get IG stories is Responding :)  \n")
-	
+    print("\033[1;32;40mThe site to get IG stories is Responding :)  \n")
+    
 else:
-	print("\033[31m Connection not established!")
-	
+    print("\033[31m Connection not established!")
+    
 soup = BeautifulSoup(r.content, "html.parser")
 
 stories = soup.find_all("div", class_ = "story", src="")
-sleep(0.5)
+nostory = soup.find("div", class_="msg msg-no-stories")
+
+sleep(0.2)
+
+
+if (bool(nostory)) == True:
+    print("This user has no Stories in the last 24h.")
+
+else:
 
 # This uses an external site to get to the Story links.
 # Media will be donwloaded from the servers of IG, which are not privacy friendly.
 
-try:
-	for story in stories:
-		file_url = story.a["onclick"]
-		replacer = file_url.replace("window.download('", "").replace("'); return false;", "")
-		file_name = replacer.split("&")[-5].replace("_nc_ohc=", "")[:7]
-		
-		#print(file_url)
-	
-		print(replacer)
-			
-		r = requests.get(replacer, stream=True)
+    try:
+        for story in stories:
+            file_url = story.a["onclick"]
+            replacer = file_url.replace("window.download('", "").replace("'); return false;", "")
+            file_name = replacer.split("&")[-5].replace("_nc_ohc=", "")[7:]
+            # Random unique file_name from the URL. Gonna improve it.
+            #print(file_url)
+        
+            print(replacer)
+                
+            r = requests.get(replacer, stream=True)
 
-		if r.status_code == 200:
-			with open(file_name, 'wb') as f:
-				for chunk in r:
-					f.write(chunk)
-				print(f"\033[33m\nGetting stories of:\033[33m @{query}")
-		else:
-			print("Cannot make connection to download media!")
-		
-except KeyError:
-	print("This user has no Stories in the last 24h.")
-except KeyboardInterrupt:
-	print("You are stoppping me...!")
-	while True:
-		sys.exit()
-else:
-	print("\nAt least one Story found. Successfully Downloaded.")
+            if r.status_code == 200:
+                with open(file_name, 'wb') as f:
+                    for chunk in r:
+                        f.write(chunk)
+                    print(f"\033[33m\nGetting stories of:\033[33m @{query}")
+            else:
+                print("Cannot make connection to download media!")
+
+
+    except KeyboardInterrupt:
+        print("You are stoppping me...!")
+        while True:
+            sys.exit()
+    else:
+        print("\nAt least one Story found. Successfully Downloaded.")
+
